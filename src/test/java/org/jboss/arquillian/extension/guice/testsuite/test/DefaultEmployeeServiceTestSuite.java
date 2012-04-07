ee8fc76d4311c81c5b5156aac20eb967d7e52e3c
Jakub Narloch <jmnarloch@gmail.com>
2012-04-07 13:24:03 +0200

    Corrected package name.

--- src/test/java/org/jboss/arquillian/extension/guice/testsuite/test/DefaultEmployeeServiceTestSuite.java
+++ src/test/java/org/jboss/arquillian/extension/guice/testsuite/test/DefaultEmployeeServiceTestSuite.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.arquillian.guice.testsuite.service.impl;
+package org.jboss.arquillian.extension.guice.testsuite.test;
 
 import com.google.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
